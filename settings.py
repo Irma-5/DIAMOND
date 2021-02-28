026a9a8d62183e4142fc4ef296bc689ccc0c8aba
AlinasFuneral <74972711+AlinasFuneral@users.noreply.github.com>
2021-02-28 23:02:58 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,29 @@
-FPS = 50
+# настройки вынесены в отдельный файл, чтобы было проще ориентироваться в коде
 
-blue = (0, 0, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
-white = (255, 255, 255)
-black = (0, 0, 0)
-orange = (255, 165, 0)
-sky_blue = (135, 206, 235)
+FPS = 30  # количество кадров
+
+# размер экрана
+WINWIDTH = 800
+WINHEIGHT = 600
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+TILEWIDTH = 50
+TILEHEIGHT = 85
+TILEFLOORHEIGHT = 40
+
+# игровые настройки
+CAM_MOVE_SPEED = 5
+OUTSIDE_DECORATION_PCT = 20
+
+# цвета
+BLUE = (0, 0, 0)
+WHITE = (255, 255, 255)
+BGCOLOR = BLUE
+TEXTCOLOR = WHITE
+
+# направление движения
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
